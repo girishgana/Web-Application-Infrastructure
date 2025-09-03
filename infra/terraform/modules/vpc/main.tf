39714fcde65fa8b292bffcd5a6f2e363787c8cd2
girishgana <128114810+girishgana@users.noreply.github.com>
2025-09-03 17:29:55 +0530

    Update main.tf

--- infra/terraform/modules/vpc/main.tf
+++ infra/terraform/modules/vpc/main.tf
@@ -5,7 +5,7 @@ terraform {
   }
 }
 
-resource "aws_vpc" "demo-vpc" {
+resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
