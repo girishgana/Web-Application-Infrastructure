042976fc375bc775c8dd50bd0aebf73102816ffe
girishgana <128114810+girishgana@users.noreply.github.com>
2025-09-19 15:13:18 +0530

    Delete infra/terraform directory

--- infra/terraform/modules/rds/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "name" { type = string }
-variable "vpc_id" { type = string }
-variable "private_subnet_ids" { type = list(string) }
-variable "eks_cluster_sg_id" { type = string }
-variable "db_name" { type = string, default = "appdb" }
-variable "db_username" { type = string, default = "appuser" }
-variable "instance_class" { type = string, default = "db.t3.medium" }
-variable "tags" { type = map(string), default = {} }
